41e63eba9d37c64123d8e6618600ed19870c490d
queenshira <queenshira15@gmail.com>
2026-01-12 10:04:50 +0700

    Tambah Validasi Nilai

--- NilaisiswaApp/Program.cs
+++ NilaisiswaApp/Program.cs
@@ -9,3 +9,7 @@ nama = Console.ReadLine();
 Console.Write("Input Nilai:  ");
 nilai = int.Parse(Console.ReadLine());
 
+if (nilai >= 75)
+    Console.WriteLine("Selamat, Anda Lulus!");
+else
+    Console.WriteLine("Maaf, Anda Belum Lulus");
